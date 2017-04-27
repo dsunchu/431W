bce6083118fc320eee5367f4acc1bbd40c0f2446
Chris Iaquinto <iaquinto.chris@gmail.com>
2017-04-27 11:23:21 -0400

    Populated home page, fixed purchase suggestions, updated search results. Added images

--- congo/database/admin.py
+++ congo/database/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import supplier
-from .models import RegisteredUser, sells, sale_items, categories, sale_items, user_list
+from .models import RegisteredUser, sells, sale_items, categories, sale_items, user_list, reviews, addresses
 
 # Register your models here.
 
@@ -10,3 +10,5 @@ admin.site.register(sells)
 admin.site.register(categories)
 admin.site.register(sale_items)
 admin.site.register(user_list)
+admin.site.register(reviews)
+admin.site.register(addresses)
